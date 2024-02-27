594fa1e79ad4c2fe47111059f0a30b17b25c2e3b
braczkot <112940576+braczkot@users.noreply.github.com>
2024-02-27 12:50:37 +0100

    Add files via upload

--- Adatkarbantartas/LoginScreen.xaml.cs
+++ Adatkarbantartas/LoginScreen.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,6 +28,20 @@ namespace Adatkarbantartas
 
         private void btnSubmit_Click(object sender, RoutedEventArgs e)
         {
+            if(txtEmail.Text == "admin@admin.com" && txtPassword.Password == "password")
+            {
+                MainWindow objWindow = new MainWindow();
+                objWindow.Show();
+                this.Close();
+            }
+            else if(txtEmail.Text == "" || txtPassword.Password == "" || (txtEmail.Text == "" && txtPassword.Password == "")) 
+            {
+                MessageBox.Show("Missing login credentials!");
+            }
+            else
+            {
+                MessageBox.Show("Wrong E-mail, and/or password!");
+            }
             
         }
     }
